f1f79d29c8a57ed730cb9cdb51d3e167c0d006a3
p18750 <david.lichtsteiner@css.ch>
2025-01-04 10:48:03 +0100

    THO: Pt2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,15 +2,20 @@ import {Component, signal, WritableSignal} from '@angular/core';
 import {UpperCasePipe} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Hero} from './heroes.types';
+import {HEROES} from '../data/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
-  hero: WritableSignal<Hero> = signal({
-    id: 1,
-    name: 'Windstorm',
-  })
+  heroes$: WritableSignal<Hero[]> = signal(HEROES);
+
+  selectedHero$: WritableSignal<Hero | null> = signal(null);
+
+  onSelect(hero: Hero): void {
+    this.selectedHero$.set(hero);
+  }
 }
